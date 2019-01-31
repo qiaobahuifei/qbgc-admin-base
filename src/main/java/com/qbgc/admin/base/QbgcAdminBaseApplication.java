0cdeff459a3c4ae0266be3c03488d5618ae12e93
gaochao <gaochao5kuba@163,com>
2019-01-31 17:37:52 +0800

    first

---
+++ src/main/java/com/qbgc/admin/base/QbgcAdminBaseApplication.java
@@ -0,0 +1,14 @@
+package com.qbgc.admin.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QbgcAdminBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QbgcAdminBaseApplication.class, args);
+	}
+
+}
+
